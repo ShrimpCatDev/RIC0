75a4de02ef209fa9da633eb482c3ca92f4361797
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-21 17:56:13 -0600

    add RAM functions

---
+++ emu/RAM.lua
@@ -0,0 +1,25 @@
+local mem={}
+
+function mem:init()
+    self.size=0xFFFF
+    self.ram={}
+    for i=1,self.size+1 do
+        self.ram[i]=0
+    end
+end
+
+function mem:poke(addr,val)
+    if addr+1>#self.ram or addr<0 or type(val)~="number" then
+        return 0
+    end
+    self.ram[addr+1] = bit.band((val or 0),0xFF)
+end
+
+function mem:peek(addr)
+    if addr+1>#self.ram or addr<0 then
+        return 0
+    end
+    return self.ram[addr+1]
+end
+
+return mem
---
+++ main.lua
@@ -0,0 +1,30 @@
+mem=require("emu/RAM")
+
+bit=require("bit")
+
+function comp(val1,val2)
+    return bit.bor(val1,bit.lshift(val2,4))
+end
+
+function decomp(val)
+    local a=bit.band(val,0xF)
+    local b=bit.band(bit.rshift(val,4),0xF)
+    return a,b
+end
+
+function love.load()
+    mem:init()
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(k)
+
+end
+
