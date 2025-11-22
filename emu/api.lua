779cfd613a400d70c0dd5caf395f2da40e74d7d7
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-21 22:05:41 -0600

    add some api things

--- emu/api.lua
+++ emu/api.lua
@@ -1,11 +1,40 @@
 local api={}
 
-function api:pset(x,y,c)
-
+function comp(val1,val2)
+    return bit.bor(val1,bit.lshift(val2,4))
 end
 
-function api:pget(x,y)
-
+function decomp(val)
+    local a=bit.band(val,0xF)
+    local b=bit.band(bit.rshift(val,4),0xF)
+    return a,b
 end
+
+function api:pget(x, y)
+    local ind = math.floor((y * sys.sw + x) / 2)
+    local hi, lo = decomp(mem:peek(ind))
+    if x % 2 == 0 then
+        return hi
+    else
+        return lo
+    end
+end
+
+
+function api:pset(x, y, c)
+    local ind = math.floor((y * sys.sw + x) / 2)
+    local hi, lo = decomp(mem:peek(ind))
+
+    if x % 2 == 0 then
+        hi = bit.band(c, 0xF)
+    else
+        lo = bit.band(c, 0xF)
+    end
+
+    mem:poke(ind, comp(hi, lo))
+end
+
+
+
 
 return api
--- main.lua
+++ main.lua
@@ -1,28 +1,57 @@
+lg=love.graphics
+
+sys={
+    sw=160,
+    sh=128
+}
+
 mem=require("emu/ram")
 api=require("emu/api")
 
+pal=require("lib/pal")
+
+pal:new("pal",love.image.newImageData("emu/data/palette.png"))
+pal:load("pal")
+
 bit=require("bit")
 
-function comp(val1,val2)
-    return bit.bor(val1,bit.lshift(val2,4))
-end
-
-function decomp(val)
-    local a=bit.band(val,0xF)
-    local b=bit.band(bit.rshift(val,4),0xF)
-    return a,b
-end
-
 function love.load()
+    lg.setDefaultFilter("nearest")
     mem:init()
+    for i=0,159 do
+        for y=0,127 do
+            api:pset(i,y,bit.band(i,y))
+        end
+    end
+    scn=love.image.newImageData(sys.sw,sys.sh)
 end
 
-function love.update()
-
+function love.update(dt)
+    mem:poke(love.math.random(0,0xffff),love.math.random(0,255))
 end
 
 function love.draw()
+    scn:mapPixel(function(x,y,r,g,b,a)
+        return pal:color(api:pget(x,y))
+    end)
+    local d=lg.newImage(scn)
+    --[[lg.scale(4,4)
 
+    local ind=0
+    for y=0,127 do
+        for x=0,(160/2)-1 do
+            local a,b=decomp(mem:peek(ind))
+
+            lg.setColor(pal:color(a))
+            lg.rectangle("fill",x*2,y,1,1)
+            lg.setColor(pal:color(b))
+            lg.rectangle("fill",x*2+1,y,1,1)
+
+            ind=ind+1
+        end
+    end]]
+    lg.draw(d,0,0,0,3,3)
+    lg.print(love.timer.getFPS(),0,200)
 end
 
 function love.keypressed(k)
